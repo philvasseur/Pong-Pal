da136628b495a0d3bf7434da84685d98cdf61080
philvasseur <pvass153@gmail.com>
2017-07-26 11:03:27 -0600

    starting file, setup state

---
+++ init.py
@@ -0,0 +1,11 @@
+import os,time,sqlite3
+from slackclient import SlackClient
+from config import BOT_TOKEN
+
+slack = SlackClient(BOT_TOKEN)
+
+if __name__ == "__main__":
+	if slack.rtm_connect():
+		print('Connected and Ready To Go!')
+		while True:
+			time.sleep(1)
