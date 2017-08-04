d477e1ba5ce1da98706f322d70cc717ce5bea269
philvasseur <pvass153@gmail.com>
2017-08-04 21:32:26 +0000

    launcher changes

--- sendMessages.py
+++ sendMessages.py
@@ -3,17 +3,12 @@ from slackclient import SlackClient
 from config import BOT_TOKEN
 slack = SlackClient(BOT_TOKEN)
 if __name__ == "__main__":
-	print('hi')
 	print(slack.rtm_connect())
 	BOT_ID = slack.api_call("auth.test").get('user_id')
-	for user in slack.api_call("users.list").get("members"):
-		slack.api_call(
-		"chat.postMessage",
-		channel=user['id'],
-		as_user = True,
-		text="If you ever want to play ping pong, just message me `status`. If you want to be notified when the room is open, type `notify`. I'll be out of service for a few minutes, but then I'll be back online! And don't worry, this is the last unsolicited message you'll receieve :smiley:"
+	slack.api_call(
+	"chat.postMessage",
+	channel="U5N89N3K2",
+	as_user = True,
+	text="Test Message To Stephen Slater"
 	)
-		print("Messaged "+ user['name']);
-	while True:
-		print("finished")
-		time.sleep(1)
+	print("Messaged Stephen");
